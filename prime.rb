a08b915680751044b5e1d17ab9d1af4a83c76ffe
Jess Rezac <jess.rezac@gmail.com>
2019-04-21 23:58:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,12 @@
 def prime?(number)
-    divisors = (2..Math.sqrt(number)).to_a
-    remainders = []
-    divisors.each do |divisor|
-      remainders.push(number % divisor)
-    end
-    if remainders.index(0) == nil
-      return true
-      puts "this was true"
-    else
-      return false
-      puts "this was false"
-    end
+  divisors = (2..Math.sqrt(number)).to_a
+  remainders = []
+  divisors.each do |divisor|
+    remainders.push(number % divisor)
+  end
+  if remainders.index(0) == nil
+    return true
+  else
+    return false
+  end
 end
-
-prime?(17)
