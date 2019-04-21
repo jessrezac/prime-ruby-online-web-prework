afcaedc9f96a58405a6bb652d8656f2709ab8a91
Jess Rezac <jess.rezac@gmail.com>
2019-04-21 23:30:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,8 @@ def prime?(number)
   divisors = (2..Math.sqrt(number)).to_a
   divisors.each do |divisor|
     remainder = number % divisor
-    if remainder > 0
-      puts "Maybe still Prime"
-    else
+    if remainder == 0
       break
-      puts "Composite"
     end
   end
 end
